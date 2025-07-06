2189d066065efcd4e24c80a2b5d1474175bf7f7b
Harshit Kumar Singh <163038346+HarshittSinghh@users.noreply.github.com>
2025-07-07 04:07:36 +0530

    Update test_Job.cc

--- tests/test_Job.cc
+++ tests/test_Job.cc
@@ -1,7 +1,7 @@
 
 #include <gtest/gtest.h>
-#include "../models/Person.h"
-#include "../models/Job.h"
+#include "./models/Person.h"
+#include "./models/Job.h"
 
 TEST(Person, Constructor) {
     Person person("Alice", 25);
